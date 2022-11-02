e369fa6c9c18c6ecd03a752e2068ae9376be447f
Felix D.H Kang(Air) <cozyfex@gmail.com>
2022-11-02 15:08:19 +0900

    Change vite server host 'lcoalhost' to '127.0.0.1'.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: { host: '127.0.0.1', port: 3000 },
   build: { minify: true },
   root: '',
   resolve: {
